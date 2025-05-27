73064ff5fbc0deb361604077e87cf54caad7e18a
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-05-27 18:50:40 +0300

    update title and desc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,23 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "اختبار مادة تكنلوجيا المعلومات والابتكار",
+  keywords: ["اختبار", "تكنلوجيا المعلومات", "ابتكار", "نظم معلومات الأعمال"],
+  authors: [{ name: "MahmoudNasr", url: "https://github.com/mahm0udnasr" }],
+  creator: "MahmoudNasr",
+  description:
+    "اختبار مادة تكنلوجيا المعلومات والابتكار - الفرقة الثانيه نظم معلومات الأعمال",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
